4c70de62fea737dd7a2d233dcb21ee5732c9506a
chris haynes <chrishaynes@chriss-mbp.home>
2019-05-18 07:42:33 -0400

    working on removing defaultMovie function to shorten my code

--- liri.js
+++ liri.js
@@ -10,7 +10,7 @@ var userCommand = process.argv[2];
 // console.log(userCommand);
 
 var userInput = process.argv.slice(3).join(" ");
-// console.log(userInput);
+console.log("userInput: ",userInput);
 
 
 //switch statement to see which api call to make//
@@ -28,82 +28,85 @@ switch (userCommand) {
         movieAPI();
         break;
 
+    
+    
+    
     default:
-        console.log("Please enter one of the following commands, spotify-this-song, concert-this, movie-this, or do-what-it-says");
-        console.log("==============================================================================================================")
+    console.log("Please enter one of the following commands, spotify-this-song, concert-this, movie-this, or do-what-it-says");
+    console.log("==============================================================================================================")
 }
 
 function bandsAPI() {
     var concertUrl = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp"
+       
+    axios.get(concertUrl).then(function (response) {
 
-    axios.get(concertUrl).then(
-        
-        function (err, response) {
-           
             var date = response.data[0].datetime;
             var formatDate = moment(date).format('MM/DD/YYYY'); 
-            
+
             console.log("====================================================================================================================================");
             console.log(userInput +"'s next concert is at " + response.data[0].venue.name);
             console.log("In " + response.data[0].venue.city + ", " + response.data[0].venue.region);
             console.log("On " + formatDate);
             console.log("====================================================================================================================================");
-            
         }
     );
 }
 
 function spotifyAPI() {
-
+  
+    if (userInput === undefined || userInput === "") {
+        userInput = "The Power of Love";
+    }  
+    
     var spotify = new Spotify(keys.spotify);
     
-    // console.log(spotify)
-
     spotify.search({ type: "track", query: userInput, limit: 1}).then(function(response) {
-        console.log(response.tracks.items);
-        console.log("====================================================================================================================================");
-         
-        
-        console.log("====================================================================================================================================");
-    
-    
-    })
-           
+        var songs = response.tracks.items[0];
+
+            console.log("====================================================================================================================================");
+            console.log("Artist(s): " + songs.artists[0].name);
+            console.log("Song Name: " + songs.name);
+            console.log("Preview Song: " + songs.preview_url);
+            console.log("Album: " + songs.album.name);
+            console.log("====================================================================================================================================");
+        });
     }
 
+// function defaultMovie() {
+//     var defaultMovieUrl = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy";
 
+//     axios.get(defaultMovieUrl).then(function (response) {
 
+//         var date = response.data.Released;
+//         var formatDate = moment(date).format('MM/DD/YYYY'); 
 
-
-
-function defaultMovie() {
-    var defaultMovieUrl = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy";
-
-    axios.get(defaultMovieUrl).then(function (response) {
-
-            console.log("====================================================================================================================================");
-            console.log("Title: " + response.data.Title);
-            console.log("Released: " + response.data.Released);
-            console.log("Rated: " + response.data.Rated);
-            // console.log("Rotten Tomatoes Rating: " + response.data.Ratings.Source.Value);
-            console.log("Country: " + response.data.Country);
-            console.log("Language: " + response.data.Language);
-            console.log("Plot: " + response.data.Plot);
-            console.log("Actors: " + response.data.Actors);
-            console.log("====================================================================================================================================");
-        }
-    );
-}
+//             console.log("====================================================================================================================================");
+//             console.log("Title: " + response.data.Title);
+//             console.log("Released: " + formatDate);
+//             console.log("Rated: " + response.data.Rated);
+//             // console.log("Rotten Tomatoes Rating: " + response.data.Ratings.Source.Value);
+//             console.log("Country: " + response.data.Country);
+//             console.log("Language: " + response.data.Language);
+//             console.log("Plot: " + response.data.Plot);
+//             console.log("Actors: " + response.data.Actors);
+//             console.log("====================================================================================================================================");
+//         }
+//     );
+// }
 
 function movieAPI() {
 
+    if (userInput === undefined || userInput === "") {
+        userInput = "Mr. Nobody";
+    }  
+    
     var movieQueryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
 
-    axios.get(movieQueryUrl).then(function (err, response) {
+    axios.get(movieQueryUrl).then(function (response) {
 
-            if (userInput === "" || err) {
-                defaultMovie()
-            } else {
+           
+            
                 console.log("====================================================================================================================================");
                 console.log("Title: " + response.data.Title);
                 console.log("Released: " + response.data.Released);
@@ -114,7 +117,7 @@ function movieAPI() {
                 console.log("Plot: " + response.data.Plot);
                 console.log("Actors: " + response.data.Actors);
                 console.log("====================================================================================================================================");
-            }
+            
         }
     );
 }
@@ -122,3 +125,22 @@ function movieAPI() {
 
 
 
+// function bandsAPI() {
+//     var concertUrl = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp"
+
+//     axios.get(concertUrl).then(
+        
+//         function (response) {
+           
+//             var date = response.data[0].datetime;
+//             var formatDate = moment(date).format('MM/DD/YYYY'); 
+            
+//             console.log("====================================================================================================================================");
+//             console.log(userInput +"'s next concert is at " + response.data[0].venue.name);
+//             console.log("In " + response.data[0].venue.city + ", " + response.data[0].venue.region);
+//             console.log("On " + formatDate);
+//             console.log("====================================================================================================================================");
+            
+//         }
+//     );
+// }
